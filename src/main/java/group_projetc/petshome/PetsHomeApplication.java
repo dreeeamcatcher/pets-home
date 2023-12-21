e8ff8217bb74318aba9ee2999ab89a44a8b0450a
Vladyslav Fesenko <vlad.11.fesenko@gmail.com>
2023-12-21 09:44:59 +0200

    first commit

---
+++ src/main/java/group_projetc/petshome/PetsHomeApplication.java
@@ -0,0 +1,13 @@
+package group_projetc.petshome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetsHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group_projetc/petshome/PetsHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package group_projetc.petshome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
