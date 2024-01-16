8fe6a16458e28ec79d6ff3a5c48e1fdef1a90a1f
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2024-01-16 18:46:40 +0200

    version 3 add method getAllFiltered

--- src/main/java/mates/petshome/dto/AnimalPostDto.java
+++ src/main/java/mates/petshome/dto/AnimalPostDto.java
@@ -1,6 +1,6 @@
-package group_projetc.petshome.dto;
+package mates.petshome.dto;
 
-import group_projetc.petshome.model.AnimalType;
+import mates.petshome.model.AnimalType;
 import lombok.Getter;
 import lombok.Setter;
 
