8836dbc928e57e357ed0a750b153d2ea6b55aab2
Vladyslav Fesenko <43932683+dreeeamcatcher@users.noreply.github.com>
2024-01-20 18:54:14 +0200

    Revert "New branch with filtered mrthod"

--- src/main/java/mates/petshome/repository/AnimalPostRepository.java
+++ src/main/java/mates/petshome/repository/AnimalPostRepository.java
@@ -3,7 +3,6 @@ package mates.petshome.repository;
 import mates.petshome.model.AnimalPost;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -14,6 +13,4 @@ public interface AnimalPostRepository extends JpaRepository<AnimalPost, Long>,
         JpaSpecificationExecutor<AnimalPost> {
     @Query("FROM AnimalPost ap left join fetch ap.postImages im")
     Page<AnimalPost> findAllWithImages(Pageable pageable);
-
-    Page<AnimalPost> findAll(Specification<AnimalPost> spec, Pageable pageable);
 }
